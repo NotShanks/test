b1e66bf8f006814c1c3216b218dccf737032172d
NotShanks <70460289+NotShanks@users.noreply.github.com>
2021-02-28 17:04:20 -0500

    Create BaseBlateTel.lua

---
+++ BaseBlateTel.lua
@@ -0,0 +1,7 @@
+local plat = Instance.new("Part")
+plat.Anchored = true
+plat.Parent = workspace
+plat.Size = Vector3.new(30,1,30)
+plat.CFrame = CFrame.new(math.random(10000,99999),5000,math.random(10000,99999))
+wait()
+player.Character.HumanoidRootPart.CFrame = plat.CFrame + Vector3.new(0, 5, 0)
