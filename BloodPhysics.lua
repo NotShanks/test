0cff2cad6629c9030bcf0a89ccef77f10ece174c
NotShanks <70460289+NotShanks@users.noreply.github.com>
2021-03-02 17:35:49 -0500

    Update BloodPhysics.lua

--- BloodPhysics.lua
+++ BloodPhysics.lua
@@ -1,3 +1,17 @@
+--fullbright
+local s = Instance.new("PointLight", game.Players.LocalPlayer.Character.Head)
+s.Brightness = .3
+s.Range = 100
+
+game.Lighting.Changed:connect(function()
+game.Lighting.TimeOfDay = "14:00:00"
+game.Lighting.FogEnd = 9999
+game.Lighting.Brightness = 2
+game.Lighting.ColorCorrection.Brightness = 0.1
+game.Lighting.ColorCorrection.Saturation = 0.1
+game.Lighting.Bloom.Intensity = 0.1
+end)
+
 -- no idea who made it
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
